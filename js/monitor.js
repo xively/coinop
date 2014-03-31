6deebea5f642c4651837f6bd248370a7f92485df
Paul Bellamy <paul.a.bellamy@gmail.com>
2014-04-01 00:02:52 +0100

    Update monitor.js

--- js/monitor.js
+++ js/monitor.js
@@ -3,7 +3,7 @@
   xively.setKey('fqC5dtaOFBByR1yQgtu3fdrUqWkiCmzHWDIogAwh56AwipDZ');
 
   // Replace with your own values  
-  var feedID        = 767865973,     // Feed ID  
+  var feedID        = 767865973;     // Feed ID  
 
   var seriesData = [ 
     {
